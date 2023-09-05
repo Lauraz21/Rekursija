198d96e2c3e2a47111f627ac75c85ef109a49392
Laura <Laura@DESKTOP-BESSB6E>
2023-09-05 23:46:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+Recursion(5);
+
+void Recursion(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    Console.WriteLine(number);
+
+    Recursion(number - 1);
+
+    Console.WriteLine(number);
+    return;
+}
+
+
+
+
+Console.WriteLine(factorial_Recursion(5));
+
+long factorial_Recursion(long number)
+{
+    if (number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        long result = number * factorial_Recursion(number - 1);
+        return result;
+    }
+}
